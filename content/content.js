de8f9e809e2185a78a146a910e1293cad58d1b99
Ramnirmal0 <nirmalramofficial@gmail.com>
2021-10-14 22:22:45 +0530

    updates/basic-template

---
+++ content/content.js
@@ -0,0 +1,84 @@
+const dashboard = {
+    "dp":"#",
+    "name" : "Nirmal Ram",
+    "title" :"developer",
+    "about":"this is about me"
+}
+
+const social_connect = [
+    {
+        "social-icon":"#",
+        "social-title":"LinkedIn",
+        "social-link":"#",
+    },   
+]
+
+const freelance_service=[
+    {
+        "image":"#",
+        "title":"Service 1",
+        "content":"this is more about it",
+    },
+
+]
+
+const techStack=[
+    {
+        "icon":"#",
+        "title":"nodejs",
+        "link":"#",
+    },
+
+]
+
+const portfolio=[
+    {
+        "image":"#",
+        "title":"ausmart",
+        "status":"live",
+        "link":"#",
+    },
+]
+
+const contribution=[
+    {
+        "title":"torque++ software",
+        "platform":"github",
+        "status":"ongoing",
+        "link":"#",
+        "message":"no permisssion to view",
+    },
+]
+
+const research =[
+    {
+        "image":"#",
+        "title":"Hashpion OS",
+        "desc":"Linux based Distros",
+    },
+]
+
+const publication =[
+    {
+        "title":"ugv landmine ",
+        "publisher":"ijtsrd",
+        "link":"#"
+    },
+]
+
+const startup=[
+    {
+        "logo":"",
+        "name":"",
+    },
+]
+
+const tech_forum =[
+    {
+        "logo":"",
+        "title":"",
+        "link":"",
+    },
+]
+
+const copyrighted = `2021 &copy; | Creator : Nirmal Ram`;
