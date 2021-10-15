d094da9493f61f53334bd0ab16f8ea1c3c1b856e
Ramnirmal0 <nirmalramofficial@gmail.com>
2021-10-15 20:10:33 +0530

    added/service

--- content/content.js
+++ content/content.js
@@ -1,59 +1,59 @@
 const dashboard = {
     "dp":"https://dummyimage.com/600x400/000/fff.png",
     "name" : "Nirmal Ram",
-    "title" :"Software Engineer - Full Stack Developer | MERN Stack | Wordpress Expert | Researcher in Robotics & IoT",
-    "about":"I worked as a freelancer for around 3-year during my college years before my first full-time job with an employer.I love to code and develop attractive, responsive, and faster web applications.My specialties include quickly learning new skills and programming languages, website optimization, and the MVC method of organizing code.My superpowers include strong knowledge in Javascript, ES6, HTML5, BootStrap, CSS3/SCSS/SASS, React.js, Node.js, Redux, Express.js, MongoDB, MySQL, PHP, Python, Java8, Appium, OpenSTF,Selenium,Swagger,Keycloak, Docker.I'm still enthusiastically grabbing onto any programming language, framework, or principle I can integrate into the coding web in my head.My other hobbies are study and Research about Space Science & Robotics, Mentoring for IoT and Robotics projects, Teaching and learning new stuff every day."
+    "title" :"Software Engineer - Full Stack Developer | Freelancer | MEARN Stack Developer | Wordpress Expert | Researcher in Robotics & IoT",
+    "about":"I worked as a freelancer for around 3-year during my college years before my first full-time job with an employer.Today i am leading few tech startups for passive income.<br><br> As a professional, I love to code and develop attractive, responsive, and faster web applications.My specialties include quickly learning new skills and programming languages, website optimization, and the MVC method of organizing code.My superpowers include strong knowledge in Javascript, ES6, HTML5, BootStrap, CSS3/SCSS/SASS, React.js, Node.js, Redux, Express.js, MongoDB, MySQL, PHP, Python, Java8, Appium, OpenSTF, Selenium, Swagger, Keycloak, Docker.<br><br>I'm still enthusiastically grabbing onto any programming language, framework, or principle I can integrate into the coding web in my head.My other hobbies are Sky Gazing, Binge Watching and Research about Space Science & Robotics, Mentoring for IoT and Robotics projects, Teaching and learning new stuff every day."
 }
 
 // get icons from https://fontawesome.com/v5.15/
 const social_connect = [
     {
-        "social-icon":"fab fa-linkedin-in",
-        "social-title":"LinkedIn",
-        "social-link":"tel:+918589838398",
-    },
-    {
         "social-icon":"fab fa-whatsapp",
         "social-title":"WhatsApp",
-        "social-link":"#",
+        "social-link":"https://wa.me/message/JGLKRCTBE63BN1",
+    },
+    {
+        "social-icon":"fab fa-linkedin-in",
+        "social-title":"LinkedIn",
+        "social-link":"https://www.linkedin.com/in/nirmalram/",
     },
     {
         "social-icon":"fab fa-google",
         "social-title":"Gmail",
-        "social-link":"#",
+        "social-link":"mailto:pineapplem3care@gmail.com",
     },   
 ]
 
 const freelance_service=[
     {
-        "image":"https://dummyimage.com/600x400/000/fff.png",
-        "title":"Service 1",
-        "content":"this is more about it",
+        "image":"https://cdn.pixabay.com/photo/2015/04/20/13/17/work-731198_960_720.jpg",
+        "title":"Web Application Development",
+        "content":"",
     },
     {
-        "image":"https://dummyimage.com/600x400/000/fff.png",
-        "title":"Service 1",
-        "content":"this is more about it",
+        "image":"https://cdn.pixabay.com/photo/2018/01/28/10/59/internet-3113279_960_720.jpg",
+        "title":"Mobile Application Development",
+        "content":"",
     },
     {
-        "image":"https://dummyimage.com/600x400/000/fff.png",
-        "title":"Service 1",
-        "content":"this is more about it",
+        "image":"https://cdn.pixabay.com/photo/2021/01/17/16/10/electronics-5925621_960_720.jpg",
+        "title":"IoT & Robotics Software Development",
+        "content":"",
     },
     {
-        "image":"https://dummyimage.com/600x400/000/fff.png",
-        "title":"Service 1",
-        "content":"this is more about it",
+        "image":"https://cdn.pixabay.com/photo/2016/10/09/08/32/digital-marketing-1725340_960_720.jpg",
+        "title":"S.E.O Setup",
+        "content":"",
     },
     {
-        "image":"https://dummyimage.com/600x400/000/fff.png",
-        "title":"Service 1",
-        "content":"this is more about it",
+        "image":"https://cdn.pixabay.com/photo/2015/02/04/15/23/nokia-623939_960_720.jpg",
+        "title":"Windows Application Development",
+        "content":"",
     },
     {
-        "image":"https://dummyimage.com/600x400/000/fff.png",
-        "title":"Service 1",
-        "content":"this is more about it",
+        "image":"https://image.freepik.com/free-vector/chat-bot-concept-illustration_114360-5522.jpg",
+        "title":"Chatbot Development",
+        "content":"",
     },
 
 ]
@@ -97,6 +97,16 @@ const techStack=[
     {
         "icon":"https://img.icons8.com/color/48/000000/cloud-function.png",
         "title":"Swagger",
+        "link":"#",
+    },
+    {
+        "icon":"https://img.icons8.com/color/48/000000/redux.png",
+        "title":"Redux",
+        "link":"#",
+    },
+    {
+        "icon":"https://img.icons8.com/color/48/000000/java-web-token.png",
+        "title":"JSON Web Token",
         "link":"#",
     },
 
--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@ var info_dashboard= document.querySelector(".info-dashboard");
 dp_dashboard.innerHTML = `<img src="${dashboard.dp}" height="200" width="200px"/>`;
 info_dashboard.innerHTML = `<h1>${dashboard.name}</h1><h4>${dashboard.title}</h4><P>Connect with</P>`
 social_connect.map(item=>{
-return info_dashboard.innerHTML += ` <a class="social-button" href="${item["social-link"]}'"><i class="${item["social-icon"]}"></i><span class="social-icons">${item["social-title"]}</span></a> `
+return info_dashboard.innerHTML += ` <a class="social-button" href="${item["social-link"]}"><i class="${item["social-icon"]}"></i><span class="social-icons">${item["social-title"]}</span></a> `
 })
 
 //about us
@@ -14,12 +14,12 @@ document.querySelector('#about-us').innerHTML = `<p>${dashboard.about}</p>`;
 
 // freelance services
 freelance_service.map(item=>{
-return document.querySelector("#freelance-service").innerHTML += `<div class="service-container"><div><img src="${item.image}" height="200px" width="200px"/></div><div class="service-title">${item.title}</div><div class="service-content">${item.content}</div></div>`
+return document.querySelector("#freelance-service").innerHTML += `<div class="service-container"><div><img src="${item.image}" height="200px" width="300px"/></div><div class="service-title">${item.title}</div><div class="service-content">${item.content}</div></div>`
 });
 
 //tech stack
 techStack.map(item=>{
-return document.querySelector('#tech-stack').innerHTML += `<div class="stack-container"><div class="stack-logo"><a href="${item.link}"><img src="${item.icon}" title="${item.title}"/></a></div><div class="stack-title">${item.title}</div></div>`
+return document.querySelector('#tech-stack').innerHTML += `<div class="stack-container"><div class="stack-logo"><a href="${item.link}"><img src="${item.icon}" title="${item.title}"/></a></div></div>`
 });
 
 //contribution
